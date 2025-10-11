83c6cd4a654afe0752b8a2e6b985ce10cb06b4b1
Ilyas Khatipov <ilyaskhat@mail.ru>
2025-10-11 15:16:33 +0300

    Fix all TODO 1.01 - 1.06 and done a creative midification

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -2,6 +2,7 @@
 
 #include "utils/resource_utils.h"
 #include "utils/timer.h"
+#include <random>
 
 
 void cg::renderer::rasterization_renderer::init()
@@ -31,7 +32,27 @@ void cg::renderer::rasterization_renderer::render()
 		return cg::color::from_float3(data.ambient);
 	};
 
-	rasterizer->clear_render_target(cg::unsigned_color{.r = 56, .g = 178, .b = 37});
+	rasterizer->clear_render_target(cg::unsigned_color{0, 0, 0});
+
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<float> dis(0.0f, 1.0f);
+
+	linalg::aliases::float3 top_color{dis(gen), dis(gen), dis(gen)};
+	linalg::aliases::float3 bottom_color{dis(gen), dis(gen), dis(gen)};
+
+	for (unsigned y = 0; y < settings->height; y++) {
+    	float t = float(y) / settings->height;
+    	linalg::aliases::float3 color = top_color * (1.0f - t) + bottom_color * t;
+
+    	cg::unsigned_color uc = cg::unsigned_color::from_float3(color);
+
+    	for (unsigned x = 0; x < settings->width; x++) {
+        	render_target->item(x, y) = uc;
+    	}	
+	}
+
+
 	for(size_t shape_id = 0;shape_id < model->get_index_buffers().size();shape_id++)
 	{
 		rasterizer->set_vertex_buffer(model->get_vertex_buffers()[shape_id]);
